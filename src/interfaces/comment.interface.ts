6f0f2437de177009be377e4beb75db93e09b00b4
rpalrod0912 <rafapr0001@gmail.com>
2024-01-25 21:07:27 +0100

    some changes

--- src/interfaces/comment.interface.ts
+++ src/interfaces/comment.interface.ts
@@ -3,6 +3,7 @@ export interface CommentInterface {
   post_id: number;
   user_id: number;
   user_image: string;
+  user_name: string;
   content: string;
   created_at: string;
   updated_at: string;
