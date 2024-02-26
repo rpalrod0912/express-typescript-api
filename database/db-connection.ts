df3cf7c81f1f8e1da48f298949d89312f0f8de98
rpalrod0912 <rafapr0001@gmail.com>
2024-02-26 19:21:46 +0100

    production test

--- database/db-connection.ts
+++ database/db-connection.ts
@@ -1,9 +1,17 @@
 const { Pool } = require("pg");
 
 export const pool = new Pool({
-  host: "localhost",
-  user: "postgres",
-  password: "postgres",
-  database: "nodeapi",
+  // host: "localhost",
+  //Production Host:
+  host: "dpg-cnedarol6cac73dj04b0-a",
+  // user: "postgres",
+  //Porduction user
+  user: "postgresnodeapi",
+  // password: "postgres",
+  //Production Password
+  password: "htcxTey9dIJ3TqrRsJVh5qngyvYGOIws",
+  // database: "nodeapi",
+  //Production database:
+  database: "nodeapi_tf9r",
   port: "5432",
 });
